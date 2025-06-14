8d94f3ebf238139222637561271da00371a5c615
Amann Malik <amannmalik@gmail.com>
2025-06-14 16:23:55 -0500

    start refactoring again

--- durable-workflow-runtime/src/test/java/com/amannmalik/workflow/runtime/EntrypointTest.java
+++ durable-workflow-runtime/src/test/java/com/amannmalik/workflow/runtime/EntrypointTest.java
@@ -1,6 +1,9 @@
 package com.amannmalik.workflow.runtime;
 
-import dev.restate.sdk.WorkflowContext;
+import com.amannmalik.workflow.runtime.cron.CronJob;
+import com.amannmalik.workflow.runtime.cron.CronJobInitiator;
+import com.amannmalik.workflow.runtime.task.WorkflowTaskService;
+import dev.restate.sdk.endpoint.Endpoint;
 import io.serverlessworkflow.api.types.Document;
 import io.serverlessworkflow.api.types.Task;
 import io.serverlessworkflow.api.types.TaskItem;
@@ -8,10 +11,9 @@ import io.serverlessworkflow.api.types.WaitTask;
 import io.serverlessworkflow.api.types.Workflow;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import java.time.Duration;
 import java.util.List;
+
 
 class EntrypointTest {
     @AfterEach
@@ -21,24 +23,25 @@ class EntrypointTest {
 
     @Test
     void testSetAndWaitTasks() {
-        WorkflowContext ctx = Mockito.mock(WorkflowContext.class);
-        Mockito.doNothing().when(ctx).sleep(Mockito.any());
+        var workflow = createTestWorkflow();
+        var builder = Endpoint.builder()
+                .bind(WorkflowRunner.DEFINITION)
+                .bind(WorkflowTaskService.DEFINITION)
+                .bind(CronJobInitiator.DEFINITION)
+                .bind(CronJob.DEFINITION);
+        var endpoint = builder.build();
+        // TODO: test if it works
+    }
 
+    private static Workflow createTestWorkflow() {
         WaitTask wt = new WaitTask();
         var to = new io.serverlessworkflow.api.types.TimeoutAfter();
         var di = new io.serverlessworkflow.api.types.DurationInline();
         di.setSeconds(1);
         to.setDurationInline(di);
         wt.setWait(to);
-
         Task task = new Task();
         task.setWaitTask(wt);
-
-        var wf = new Workflow(new Document(), List.of(new TaskItem("w", task)));
-
-        WorkflowRunner ep = new WorkflowRunner();
-        ep.run(ctx, wf);
-
-        Mockito.verify(ctx).sleep(Duration.ofSeconds(1));
+        return new Workflow(new Document(), List.of(new TaskItem("w", task)));
     }
 }
