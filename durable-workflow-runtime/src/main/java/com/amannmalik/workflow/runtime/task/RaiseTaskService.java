090f63779d328fd07990c281c2cfa7d9fa0c14f9
Amann Malik <amannmalik@gmail.com>
2025-06-15 11:04:09 -0500

    Implement DSL-compliant try/catch and raise

---
+++ durable-workflow-runtime/src/main/java/com/amannmalik/workflow/runtime/task/RaiseTaskService.java
@@ -0,0 +1,34 @@
+package com.amannmalik.workflow.runtime.task;
+
+import com.amannmalik.workflow.runtime.DefinitionHelper;
+import dev.restate.sdk.WorkflowContext;
+import dev.restate.sdk.endpoint.definition.ServiceDefinition;
+import io.serverlessworkflow.api.types.Error;
+import io.serverlessworkflow.api.types.RaiseTask;
+import io.serverlessworkflow.api.types.RaiseTaskConfiguration;
+import io.serverlessworkflow.api.types.RaiseTaskError;
+
+/** Service executing the raise task as defined in the DSL. */
+public class RaiseTaskService {
+
+    public static final ServiceDefinition DEFINITION = DefinitionHelper.taskService(
+            RaiseTaskService.class,
+            RaiseTask.class,
+            RaiseTaskService::execute
+    );
+
+    public static void execute(WorkflowContext ctx, RaiseTask task) {
+        RaiseTaskConfiguration rc = task.getRaise();
+        if (rc == null || rc.getError() == null) {
+            return;
+        }
+        RaiseTaskError rte = rc.getError();
+        Object val = rte.get();
+        if (val instanceof Error err) {
+            throw new WorkflowErrorException(err);
+        } else {
+            // References to reusable errors not yet supported
+            throw new RuntimeException("Referenced errors not supported");
+        }
+    }
+}
--- durable-workflow-runtime/src/main/java/com/amannmalik/workflow/runtime/task/TryTaskService.java
+++ durable-workflow-runtime/src/main/java/com/amannmalik/workflow/runtime/task/TryTaskService.java
@@ -1,11 +1,16 @@
 package com.amannmalik.workflow.runtime.task;
 
 import dev.restate.sdk.WorkflowContext;
+import dev.restate.sdk.common.StateKey;
 import dev.restate.sdk.endpoint.definition.ServiceDefinition;
 import com.amannmalik.workflow.runtime.DefinitionHelper;
 import io.serverlessworkflow.api.types.TaskItem;
 import io.serverlessworkflow.api.types.TryTask;
 import io.serverlessworkflow.api.types.TryTaskCatch;
+import io.serverlessworkflow.api.types.ErrorFilter;
+import io.serverlessworkflow.api.types.Error;
+
+import com.amannmalik.workflow.runtime.task.WorkflowErrorException;
 
 import java.util.List;
 
@@ -25,13 +30,63 @@ public class TryTaskService {
                 new WorkflowTaskService().execute(ctx, ti.getTask());
             }
         } catch (Exception e) {
-            if (aCatch != null) {
-                for (var ti : aCatch.getDo()) {
-                    new WorkflowTaskService().execute(ctx, ti.getTask());
+            if (aCatch == null) {
+                throw e;
+            }
+
+            if (e instanceof WorkflowErrorException we) {
+                if (!matches(aCatch.getErrors() == null ? null : aCatch.getErrors().getWith(), we.getError())) {
+                    throw e;
                 }
+                String var = aCatch.getAs() == null ? "error" : aCatch.getAs();
+                ctx.set(StateKey.of(var, Error.class), we.getError());
             } else {
                 throw e;
             }
+
+            if (aCatch.getDo() != null) {
+                for (var ti : aCatch.getDo()) {
+                    new WorkflowTaskService().execute(ctx, ti.getTask());
+                }
+            }
         }
+    }
+
+    private static boolean matches(ErrorFilter filter, Error err) {
+        if (filter == null) {
+            return true;
+        }
+        if (filter.getType() != null) {
+            var et = err.getType();
+            String t = et == null ? null : et.getLiteralErrorType().toString();
+            if (!filter.getType().equals(t)) {
+                return false;
+            }
+        }
+        if (filter.getStatus() != 0 && filter.getStatus() != err.getStatus()) {
+            return false;
+        }
+        if (filter.getInstance() != null) {
+            var inst = err.getInstance();
+            String i = inst == null ? null : inst.getLiteralErrorInstance();
+            if (!filter.getInstance().equals(i)) {
+                return false;
+            }
+        }
+        if (filter.getTitle() != null) {
+            var tt = err.getTitle();
+            String t = tt == null ? null : tt.getLiteralErrorTitle();
+            if (!filter.getTitle().equals(t)) {
+                return false;
+            }
+        }
+        if (filter.getDetails() != null) {
+            var dt = err.getDetail();
+            String d = dt == null ? null : dt.getLiteralErrorDetails();
+            if (!filter.getDetails().equals(d)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
