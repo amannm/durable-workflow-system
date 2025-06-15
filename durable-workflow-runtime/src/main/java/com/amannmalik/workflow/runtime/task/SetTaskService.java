2311ffa87c2774e76730ba633ae6f0d98ac289db
Amann Malik <amannmalik@gmail.com>
2025-06-14 20:40:25 -0500

    Support map values in set task

--- durable-workflow-runtime/src/main/java/com/amannmalik/workflow/runtime/task/SetTaskService.java
+++ durable-workflow-runtime/src/main/java/com/amannmalik/workflow/runtime/task/SetTaskService.java
@@ -20,15 +20,27 @@ public class SetTaskService {
 
     public static void execute(WorkflowContext ctx, SetTask task) {
         var s = task.getSet();
+        var config = s.getSetTaskConfiguration();
+        if (config != null) {
+            config.getAdditionalProperties().forEach((k, v) -> setValue(ctx, k, v));
+            return;
+        }
+
         var k = s.getString();
-        var v = s.get();
+        if (k != null) {
+            // Treat single string as key name and value
+            ctx.set(StateKey.of(k, String.class), k);
+        }
+    }
+
+    private static void setValue(WorkflowContext ctx, String k, Object v) {
         switch (v) {
             case String sv -> ctx.set(StateKey.of(k, String.class), sv);
             case Long sl -> ctx.set(StateKey.of(k, Long.class), sl);
             case Integer si -> ctx.set(StateKey.of(k, Integer.class), si);
             case Double sd -> ctx.set(StateKey.of(k, Double.class), sd);
             case Boolean sb -> ctx.set(StateKey.of(k, Boolean.class), sb);
-            default -> log.warn("Unsupported set value type: {}", v.getClass());
+            default -> ctx.set(StateKey.of(k, Object.class), v);
         }
     }
 }
